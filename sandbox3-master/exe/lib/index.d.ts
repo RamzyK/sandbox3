d9b87a9af131f394a464d8864ef8b451af03f4ff
Kermad <gkermad.ramzy@gmail.com>
2018-04-17 10:08:35 +0200

    init

---
+++ sandbox3-master/exe/lib/index.d.ts
@@ -0,0 +1,2 @@
+export { IDocFusionOptions } from './doc-fusion/fusion-options';
+export { DocFusion } from './doc-fusion/doc-fusion';
---
+++ sandbox3-master/exe/lib/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var doc_fusion_1 = require("./doc-fusion/doc-fusion");
+exports.DocFusion = doc_fusion_1.DocFusion;
+//# sourceMappingURL=index.js.map
